cf4352e7ba1a44bcc209a376e25be3f77661998a
Imran Rafique <imran.rafique@axiomworld.net>
2023-09-12 14:58:36 +0500

    [FIX] * Name

--- multi_classes_ids/__manifest__.py
+++ multi_classes_ids/__manifest__.py
@@ -1,5 +1,5 @@
 {
-    "name": "Multi Classes IDs for Any Section || Multi Classes || Multi IDs || Custom Styles || Custom CSS",
+    "name": "Multi Classes IDs for Any Section || Multi Classes || IDs || Custom Styles || Custom CSS",
     "summary": "Multi Classes, IDs and Custom Styles (CSS) for any Section, Div and Img Tags for any Snippet",
     "description": "Now its easy to add Multiple Classes, IDs and Custom Styles Styles (CSS) for any Section, Div and Img Tags on any Snippet",
     "category": "classes/ids/style",
