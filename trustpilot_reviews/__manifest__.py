6e406330fd9684557b6fb67a9b5f79f8503a2e0d
imran.rafique <imran.rafique@axiomworld.net>
2026-03-05 20:41:40 +0500

    [UPDATE] * __manifest__

--- trustpilot_reviews/__manifest__.py
+++ trustpilot_reviews/__manifest__.py
@@ -32,7 +32,7 @@
             "trustpilot_reviews/static/src/js/**",
         ]
     },
-    "images": ["icon.png", "icon.png"],
+    "images": ['static/description/banner.png', 'static/description/icon.png'],
     "price": "30",
     "currency": "EUR",
     "installable": True,
